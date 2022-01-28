7352a7f70f809409bf1c4a3c38eab908c984877a
Matthieu Bucchianeri <mbucchia@gmail.com>
2022-01-27 19:25:58 -0800

    Branch for Beta-2 release.

--- XR_APILAYER_NOVENDOR_toolkit/layer.h
+++ XR_APILAYER_NOVENDOR_toolkit/layer.h
@@ -28,9 +28,9 @@ namespace toolkit {
 
     const std::string LayerName = "XR_APILAYER_NOVENDOR_toolkit";
     const uint32_t VersionMajor = 0;
-    const uint32_t VersionMinor = 0;
-    const uint32_t VersionPatch = 0;
-    const std::string VersionString = "Unreleased";
+    const uint32_t VersionMinor = 9;
+    const uint32_t VersionPatch = 4;
+    const std::string VersionString = "Beta-2";
     const std::string RegPrefix = "SOFTWARE\\OpenXR_Toolkit";
 
     // Singleton accessor.
