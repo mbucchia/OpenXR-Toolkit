983416bf9f5bbc5f937d9833145251af1ba49d13
CptLucky8 <96778648+CptLucky8@users.noreply.github.com>
2022-02-28 21:30:21 -0500

    Update layer.cpp
    Add TODO comment prior merging.

--- XR_APILAYER_NOVENDOR_toolkit/layer.cpp
+++ XR_APILAYER_NOVENDOR_toolkit/layer.cpp
@@ -1556,6 +1556,8 @@ namespace {
                 m_configManager->getValue(config::SettingScreenshotEnabled);
 
             if (textureForOverlay[0] && requestScreenshot) {
+                // TODO: this is capturing frame N-3
+                // review the command queues/lists and context flush
                 takeScreenshot(textureForOverlay[0]);
 
 #ifdef _DEBUG
