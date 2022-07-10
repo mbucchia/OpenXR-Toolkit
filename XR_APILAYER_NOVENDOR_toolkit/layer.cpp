a08a4490216f12e7c677ff38cac3bdc98cf33e08
Matthieu Bucchianeri <mbucchia@gmail.com>
2022-07-10 13:36:07 -0700

    Disable intercetor with RE2.

--- XR_APILAYER_NOVENDOR_toolkit/layer.cpp
+++ XR_APILAYER_NOVENDOR_toolkit/layer.cpp
@@ -196,9 +196,9 @@ namespace {
             );
             // We disable the API interceptor with certain games where it seems to cause issues. As a result, foveated
             // rendering will not be offered.
-            m_configManager->setDefault(
-                config::SettingDisableInterceptor,
-                (m_applicationName == "OpenComposite_AC2-Win64-Shipping" || m_applicationName == "OpenComposite_Il-2"));
+            m_configManager->setDefault(config::SettingDisableInterceptor,
+                                        (m_applicationName == "OpenComposite_AC2-Win64-Shipping" ||
+                                         m_applicationName == "OpenComposite_Il-2" || m_applicationName == "re2"));
             // We disable the frame analyzer when using OpenComposite, because the app does not see the OpenXR
             // textures anyways.
             m_configManager->setDefault("disable_frame_analyzer", m_isOpenComposite);
